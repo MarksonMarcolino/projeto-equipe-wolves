e33f4c1e3e112a97fcd12d95d4b20541165d8716
vinic <vinic@10.32.1.197>
2018-03-13 14:16:15 -0300

    Ceriada classe Singleton Aluno e Professor.

---
+++ src/br/com/satc/singleton/SProfessor.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.satc.singleton;
+
+import br.com.satc.objetos.Professor;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author vinic
+ */
+public class SProfessor {
+    
+    private SProfessor() {
+    }
+    private List <Professor> professores = new ArrayList();
+
+    public List<Professor> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(List<Professor> professores) {
+        this.professores = professores;
+    }
+    
+    public static SProfessor getInstance() {
+        return SProfessorHolder.INSTANCE;
+    }
+    
+    private static class SProfessorHolder {
+
+        private static final SProfessor INSTANCE = new SProfessor();
+    }
+}
